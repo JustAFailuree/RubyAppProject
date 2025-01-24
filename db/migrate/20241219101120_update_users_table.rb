a49374a17eca3f8bb7bbcf35f0f0670f36e05924
JustAFailure <68466522+JustAFailuree@users.noreply.github.com>
2025-01-24 14:08:14 +0100

    Update 20241219101120_update_users_table.rb

--- db/migrate/20241219101120_update_users_table.rb
+++ db/migrate/20241219101120_update_users_table.rb
@@ -1,6 +1,5 @@
 class UpdateUsersTable < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :last_name, :string
     remove_column :users, :role, :string
     remove_column :users, :password_digest, :string
   end
